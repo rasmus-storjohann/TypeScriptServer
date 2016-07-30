45d3ff50cfb14adb9ac6a043cab6d3d9625c684d
rasmus <rasmus.gwca@gmail.com>
2016-07-30 11:24:07 -0700

    Stubbed out server in app.ts

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+/// <reference path="_all.d.ts" />
+
+"use strict";
+
+import * as bodyParser from "body-parser";
+import * as express from "express";
+import * as path from "path";
+
+class Server {
+    public app: express.Application;
+    
+    public static bootstrap() Server {
+        return new Server();
+    }
+    
+    constructor() {
+        this.app = express();
+        this.config();
+    }
+}
+
