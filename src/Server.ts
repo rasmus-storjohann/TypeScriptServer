606b04696b14bcf04be3f4c887f700771fa86950
rasmus <rasmus.gwca@gmail.com>
2016-08-04 21:29:19 -0700

    Moved express server logic back into Server

--- src/Server.ts
+++ src/Server.ts
@@ -6,20 +6,35 @@ import * as expressAdapter from "./adapters/ExpressAdapter";
 import * as contactRepository from "./adapters/ContactRepository";
 import * as contactService from "./businessObjects/ContactService";
 
+import * as bodyParser from "body-parser";
+import * as express from "express";
+import * as path from "path";
+
 export class Server {
+  private _application: express.Application;
+  private _port: number;
+
   private _expressAdapter: expressAdapter.ExpressAdapter;
   private _contactRepository: contactRepository.ContactRepository;
   private _contactService: contactService.ContactService;
 
   constructor() {
-    var port = 3000;
-
-    this._expressAdapter = new expressAdapter.ExpressAdapter(port);
+    this._expressAdapter = new expressAdapter.ExpressAdapter();
     this._contactRepository = new contactRepository.ContactRepository;
     this._contactService = new contactService.ContactService(this._contactRepository, this._expressAdapter);
+
+    this._port = 3000;
+    this._application = express();
+
+    this._application.get("/", function(req, res) {
+      console.log("Sending Hello World!");
+      res.send("Hello World!");
+    });
   }
 
   public listen() {
-    this._expressAdapter.listen();
+    this._application.listen(this._port, function () {
+        console.log("Example app listening on port 3000!");
+    });
   }
 }
--- src/adapters/ExpressAdapter.ts
+++ src/adapters/ExpressAdapter.ts
@@ -4,27 +4,5 @@
 
 import { IWebAdapter } from "../businessInterfaces/IWebAdapter";
 
-import * as bodyParser from "body-parser";
-import * as express from "express";
-import * as path from "path";
-
 export class ExpressAdapter implements IWebAdapter {
-    private _application: express.Application;
-    private _port: number;
-
-    constructor(port: number) {
-      this._port = port;
-      this._application = express();
-
-      this._application.get("/", function(req, res) {
-        console.log("Sending Hello World!");
-        res.send("Hello World!");
-      });
-    }
-
-    public listen() {
-      this._application.listen(this._port, function () {
-          console.log("Example app listening on port 3000!");
-      });
-    }
 }
