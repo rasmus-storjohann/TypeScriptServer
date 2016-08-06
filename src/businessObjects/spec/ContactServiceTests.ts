162972658c203ef4c5f5abef6cac26f58183afc6
rasmus <rasmus.gwca@gmail.com>
2016-08-06 14:56:39 -0700

    Cleaned up unit tests

--- src/businessObjects/spec/ContactServiceTests.ts
+++ src/businessObjects/spec/ContactServiceTests.ts
@@ -1,24 +1,32 @@
+"use strict";
+
 import * as typeMoq from "typemoq";
 import * as chai from "chai";
-import * as contactService from "../ContactService";
-import * as contactRepository from "../../adapters/ContactRepository";
+
+import { ContactService } from "../ContactService";
+import { Contact } from "../../businessInterfaces/Contact";
+import { MockContactRepository } from "./MockContactRepository";
 
 describe("ContactService", () => {
-    var subject: contactService.ContactService;
-    var mockRepository: typeMoq.Mock<contactRepository.ContactRepository>;
+    var subject: ContactService;
+    var mockRepository: typeMoq.Mock<MockContactRepository>;
 
     beforeEach(function () {
-      mockRepository = typeMoq.Mock.ofType(contactRepository.ContactRepository);
-      subject = new contactService.ContactService(mockRepository.object);
+      mockRepository = typeMoq.Mock.ofType(MockContactRepository);
+      subject = new ContactService(mockRepository.object);
     });
 
     describe("#loadAllContacts", () => {
+
       it("should call loadAllContacts on the repository", () => {
+
         subject.loadAllContacts();
+
         mockRepository.verify(x => x.loadAllContacts(), typeMoq.Times.once());
       });
 
       it("should return value received from the repository", () => {
+
         var someContacts = [{ _id: 5, _firstName: "foo", _lastName: "bar", _star: true },
                             { _id: 5, _firstName: "foo2", _lastName: "bar2", _star: false }];
 
---
+++ src/businessObjects/spec/MockContactRepository.ts
@@ -0,0 +1,29 @@
+"use strict";
+
+import { IContactRepository } from "../../businessInterfaces/IContactRepository";
+import { Contact } from "../../businessInterfaces/Contact";
+
+export class MockContactRepository implements IContactRepository {
+
+  private _contact = new Contact(0, "", "", false);
+
+  saveContact(contact: Contact) {
+    return true;
+  }
+
+  updateContact(id: number, contact: Contact) {
+    return true;
+  }
+
+  deleteContact(id: number) {
+    return true;
+  }
+
+  loadAllContacts() {
+    return [ this._contact ];
+  }
+
+  loadContact(id: number) {
+    return this._contact;
+  }
+}
